454027ef73e4903cbadf56de770d510910c3650c
dconn20 <damien.connolly85@gmail.com>
2021-01-06 23:06:17 +0000

    Server&HTML working

--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+# Adapted from datarepresentation
+
 from flask import Flask
 
 from tensorflow import keras
@@ -5,13 +7,13 @@ from tensorflow import keras
 # Load model
 model = keras.models.load_model("model.h5")
 
-app = Flask(__name__, static_url_path='', static_folder='static')
+app = Flask(__name__, static_url_path='', static_folder='staticpages')
 
 @app.route("/")
 def home():
   return app.send_static_file('index.html')
 
-
+# Test with curl "http://127.0.0.1:5000/predict/11
 @app.route('/predict/<int:x>', methods=['GET'])
 @app.route('/predict/<float:x>', methods=['GET'])
 def predict(x):
